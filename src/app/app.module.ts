217f10c211a1868bb4a403ae394b19e683f9a162
oletendart <letendartolivia@gmail.com>
2020-09-24 12:48:53 +0200

    Create component cards contains differents cards descriptions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, CarouselComponent ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, CarouselComponent, CardsComponent ],
   bootstrap:    [ AppComponent, NavbarComponent ]
 })
 export class AppModule {}
