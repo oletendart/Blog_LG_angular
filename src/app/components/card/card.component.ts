f8d4289c0c0583834e357f72f91c5ee70e050b0c
oletendart <letendartolivia@gmail.com>
2020-09-26 16:04:59 +0200

    create function arrow + insert information in the array in the file card.component.ts

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
+card: Card[] = Card [ new Card('Meneur', 'Le meneur définit la composition de la partie et tire les cartes événements. Il est le maître des joueurs qui sont à sa merci.', 'https://cdn.discordapp.com/attachments/671802972329607198/758312826321895445/image0.jpg')]
   constructor() { }
 
   ngOnInit() {
