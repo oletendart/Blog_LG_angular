42621f40a8ff54c627d4e6ba5123425b03fa3fd6
oletendart <letendartolivia@gmail.com>
2020-09-29 19:18:11 +0200

    Comment out the card table in the file card.component.ts

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,7 +7,7 @@ import { Card } from '../../models/card';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-card: Card[] = Card [ new Card('Meneur', 'Le meneur définit la composition de la partie et tire les cartes événements. Il est le maître des joueurs qui sont à sa merci.', 'https://cdn.discordapp.com/attachments/671802972329607198/758312826321895445/image0.jpg')]
+/*card: Card[] = Card [ new Card('Meneur', 'Le meneur définit la composition de la partie et tire les cartes événements. Il est le maître des joueurs qui sont à sa merci.', 'https://cdn.discordapp.com/attachments/671802972329607198/758312826321895445/image0.jpg')] */
   constructor() { }
 
   ngOnInit() {
